7f6d536bc3ec002bab66cfb70918984c0665f4c1
KUrosawa,Akira <akirak@aqua.plala.or.jp>
2020-12-26 03:58:09 +0900

    続き

--- my.py
+++ my.py
@@ -4,3 +4,53 @@ print('Test Code')
 a=5
 b = a + 2
 print(b)
+
+hensuu = 6
+iremono = hensuu + 4
+print(iremono)
+
+nedan = 800
+kosuu = 3
+syuppi = nedan * kosuu
+print(syuppi)
+
+a = -3
+b = a + 4
+print(b)
+
+a = 5
+b = "こんにちは。"
+print(b)
+
+a = 5
+b = "ABCD"
+print(b)
+
+a = 5
+b = "a"
+print(b)
+
+b = "こんにちわ。"
+print("b")
+
+
+a = 5
+b = "a"
+print("b")
+print(b)
+
+a = 5
+b = "a"
+print("b")
+print(b)
+print(a)
+
+A = 1
+a = 3
+print("A=", A)
+print("a=", a)
+
+#https://ja.wikibooks.org/wiki/Python/数値入力と文字入力と出力表示
+print("文字を入力してみよう。")
+x = input()
+print(2 * x)
